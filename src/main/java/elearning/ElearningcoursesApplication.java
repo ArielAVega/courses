0f7a5a907629c40e1732cd4cbddcbc0a77449799
ArielAVega <141455765+ArielAVega@users.noreply.github.com>
2024-10-15 16:49:01 -0300

    Generación del proyecto Spring Boot

---
+++ src/main/java/elearning/ElearningcoursesApplication.java
@@ -0,0 +1,13 @@
+package elearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningcoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearningcoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/elearning/ElearningcoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningcoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
